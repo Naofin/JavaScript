4e618d56e6d9840dd85c6cbf06d86fc46e995416
Md. Naofin Hassan <naofin.081@gmail.com>
2025-11-22 12:27:37 +0600

    switch

---
+++ switch.js
@@ -0,0 +1,26 @@
+const dayOfWeek = 8;
+switch (dayOfWeek) {
+  case 1:
+    console.log('Saturday');
+    break;
+  case 2:
+    console.log('Sunday');
+    break;
+  case 3:
+    console.log('Monday');
+    break;
+  case 4:
+    console.log('Tuesday');
+    break;
+  case 5:
+    console.log('Wednesday');
+    break;
+  case 6:
+    console.log('Thursday');
+    break;
+  case 7:
+    console.log('Friday');
+    break;
+  default:
+    console.log('Invalied');
+}
