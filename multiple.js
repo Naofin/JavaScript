cc1092ea70502d2709cff7ba432b9aa1131b263e
Md. Naofin Hassan <naofin.081@gmail.com>
2025-11-22 12:27:08 +0600

    multi

---
+++ multiple.js
@@ -0,0 +1,18 @@
+let burger_price = 600;
+if (burger_price > 500) {
+  console.log('free coke')
+}
+else
+  console.log('30tk')
+
+const number = 10;
+if (number > 7) {
+  console.log('somthing...')
+}
+console.log('nothing...')
+
+let num = 0;
+while (num < 5) {
+  console.log('looooooooooooooping...');
+  num = num + 1;
+}
